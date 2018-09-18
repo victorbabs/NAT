94b6411637ee51035bc16980c9d8fb8ba7ca890e
seyex <seyibello2012@gmail.com>
2018-09-18 12:00:15 +0200

    made changes

--- jsonextract.py
+++ jsonextract.py
@@ -35,8 +35,33 @@ for eachkey in keys:
 
     connsuniquecountpertime[eachkey] = unique_ip_port_count
 
-print(connsuniquecountpertime)
+#print(connsuniquecountpertime)
+
 
 
 
 unique_ip_port_count_details = {}
+for eachtimerange in keys:
+    unique_ip_port_count_details[eachtimerange] = dict.fromkeys(list(unique_ip_port), {})
+
+print(unique_ip_port_count_details)
+
+for eachtimerange in keys:
+    #print (eachtimerange)
+    uniqueportspertimerange = connsuniquepertime[eachtimerange]
+    #print (uniqueportspertimerange)
+    entriesforthattime = contents[eachtimerange]
+    #print (len(entriesforthattime))
+    for eachentry in entriesforthattime:
+        #print (eachentry)
+        eachuniquedst = eachentry['dst1'] + ":" + eachentry['dport1']
+        eachuniquesrc = eachentry['src1'] + ":" + eachentry['sport1']
+        #print(eachuniquedst, eachuniquesrc)
+        try:
+            unique_ip_port_count_details[eachtimerange][eachuniquedst][eachuniquesrc] += 1
+            print ("An exception did not occur")
+        except:
+            unique_ip_port_count_details[eachtimerange][eachuniquedst][eachuniquesrc] = 1
+            print ("An exception occur")
+
+        print(unique_ip_port_count_details)
